cd22c1b9c8b02bb00e3ef47fd78b26396b5af8d3
Gilberto <gmorales@outlook.com.br>
2024-02-12 16:44:58 -0300

    [FEAT] - Navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,29 +1,16 @@
-import React from "react";
-
-const Navlinks = [
-
-  {
-    name: "Home",
-    path: "#sobre",
-  },
-
-  {
-    name: "Projects",
-    path: "#projetos",
-  },
-
-  {
-    name: "Contact",
-    path: "#contato",
-  }
-
-]
-
+import React from 'react';
 
 const Navbar = () => {
-
   return (
-    <div>askdhaskd</div>
-  )
-}
+    <header className="header flex items-center justify-between px-6 py-4">
+      <span className="text-black text-xl font-bold">gilbertomorales.</span>
+      <nav className="header-menu flex gap-8">
+        <a href="#sobre" className="text-gray-800 text-lg font-semibold">Home</a>
+        <a href="#projetos" className="text-gray-800 text-lg font-semibold">Projects</a>
+        <a href="#contato" className="text-gray-800 text-lg font-semibold">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
 export default Navbar;

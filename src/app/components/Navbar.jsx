6d74908341ce422f7306354657f69dc3a4666ef5
Gilberto <gmorales@outlook.com.br>
2024-02-12 00:33:38 -0300

    [FEAT] - Navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navlinks = [
+
+  {
+    name: "Home",
+    path: "#sobre",
+  },
+
+  {
+    name: "Projects",
+    path: "#projetos",
+  },
+
+  {
+    name: "Contact",
+    path: "#contato",
+  }
+
+]
+
+
+const Navbar = () => {
+
+  return (
+    <div>askdhaskd</div>
+  )
+}
+export default Navbar;

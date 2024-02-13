074474b2501307f9d36559adaeb8bc5cd9786bf4
Gilberto <gmorales@outlook.com.br>
2024-02-13 02:58:24 -0300

    [FEAT] - Responsive Layout 2/2

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div id="contato" className="contato flex justify-between items-center mx-auto max-w-6xl mt-12 mb-20">
+    <div id="contato" className="contato flex flex-col md:flex-row justify-between items-center mx-auto max-w-6xl mt-12 mb-20">
 
-      <div>
-        <img id="contato-img" src="assets/contact.svg" alt="contato" className="max-w-lg h-auto" />
+      <div className="md:mr-8">
+        <img id="contato-img" src="assets/contact.svg" alt="contato" className="w-64 md:w-auto h-auto" />
       </div>
 
-      <div className='mt-52 ml-24'>
+      <div className='mt-8 flex flex-col items-center md:items-start md:mt-40'>
 
-        <h1 className="text-7xl text-gray-950">Reach me <strong>here</strong>:</h1>
+        <h1 className="text-4xl md:text-7xl text-gray-950 mb-4">Reach me <strong>here</strong>:</h1>
 
-        <div className="flex mt-3 mb-5 gap-10">
-        <a href="mailto:eu@gilbertomorales.com">
-    <img src="assets/email.svg" alt="Email" className='max-w-24'/>
-  </a>
-  <a href="https://github.com/eumorales" target="_blank">
-    <img src="assets/github.svg" alt="Github" className='max-w-20 mt-2'/>
-  </a>
+        <div className="flex">
+          <a href="mailto:eu@gilbertomorales.com">
+            <img src="assets/email.svg" alt="Email" className='w-20 h-auto mr-4'/>
+          </a>
+          <a href="https://github.com/eumorales" target="_blank">
+            <img src="assets/github.svg" alt="Github" className='w-20 h-auto'/>
+          </a>
         </div>
       </div>
     </div>

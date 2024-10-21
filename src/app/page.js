6ea6fc6392f0fa2faa4be83ec6df8634ab08a773
Gilberto Morales <gmorales@outlook.com.br>
2024-10-21 13:13:23 -0300

    [FEAT] - Stats (WIP)

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,22 @@
+"use client";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
+import Stats from "./components/Stats";
+
 export default function Home() {
-
-    return(
-
-      <main className="flex min-h-screen flex-col bg-white">
-              <Navbar/>
-
-              <div className="container mx-auto px-12 py-4">
-
-                <About/>
-                <Projects/>
-                <Contact/>
-
-              </div>
-
-              <Footer/>
-
-        </main>
-      
-
-    );
+  return (
+    <main className="flex min-h-screen flex-col bg-white">
+      <Navbar />
+      <div className="container mx-auto px-12 py-4">
+        <About />
+        <Stats />
+        <Projects />
+        <Contact />
+      </div>
+      <Footer />
+    </main>
+  );
 }
